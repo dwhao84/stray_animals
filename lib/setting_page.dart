ebc8fe8eb7bfb9c945b03f63dc9893cf91260dbb
Dawei Hao <dwsamurai84@gmail.com>
2025-03-12 23:17:09 +0800

    Add launch Url and adjust ListView.separated

--- lib/setting_page.dart
+++ lib/setting_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'setting.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({super.key});
@@ -13,35 +15,31 @@ class _SettingPageState extends State<SettingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.description),
-              title: Text("Disclaimer"),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.bug_report),
-              title: Text("Issue Report"),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.share),
-              title: Text("Share your feedback"),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.quickreply),
-              title: Text("Check out more apps"),
-            ),
-          ),
-          Card(child: ListTile(title: Text("Dark Mode"))),
-        ],
+      body: ListView.separated(
+        itemCount: settings.length,
+        separatorBuilder: (context, index) => const Divider(),
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading:
+                settings[index].icon != null
+                    ? Icon(settings[index].icon)
+                    : null,
+            title: Text(settings[index].title),
+            onTap: () {
+              if (settings[index].url.isNotEmpty) {
+                _launchUrl(settings[index].url);
+              }
+            },
+          );
+        },
       ),
     );
+  }
+
+  Future<void> _launchUrl(String urlString) async {
+    final Uri uri = Uri.parse(urlString);
+    if (!await launchUrl(uri)) {
+      throw Exception('Could not launch $urlString');
+    }
   }
 }
